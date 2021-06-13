60ff1f95e3c49e306dbfaaa14bc2c74feb9071a9
yamakit <yamakitta@gmail.com>
2021-06-13 17:48:06 +0900

    PATHの変更・SQL文ｊにyolo_flagを追加

--- Python/upload.py
+++ Python/upload.py
@@ -15,7 +15,7 @@ for row in rows:
         files.remove(row[0])
 
 for file in files:
-    cur.execute("INSERT yolo_video_table VALUES (%(id)s, %(ans_id)s, %(ball_id)s, %(frame1)s, %(frame2)s, %(player_id)s, %(video_path)s, %(x_coordinate)s, %(y_coordinate)s, %(yolo_flag)s)", {'id':None, 'ans_id':-1, 'ball_id':1, 'frame1':-1, 'frame2':-1, 'player_id':1, 'video_path':file, 'x_coordinate':-1, 'y_coordinate':-1, 'yolo_flag':0})
+    cur.execute("INSERT yolo_video_table VALUES (%(id)s, %(ans_id)s, %(ball_id)s, %(frame1)s, %(frame2)s, %(player_id)s, %(x_coordinate)s, %(y_coordinate)s, %(video_path)s, %(yolo_flag)s)", {'id':None, 'ans_id':-1, 'ball_id':1, 'frame1':-1, 'frame2':-1, 'player_id':1, 'video_path':file, 'x_coordinate':-1, 'y_coordinate':-1, 'yolo_flag':0})
 
 print(str(files) + ' was uploaded!')
 
