10e01dd35da32e558bec9deec14cff0f33cafd3c
yamakit <yamakitta@gmail.com>
2021-06-20 13:11:16 +0900

    globの取得をjsonからMOVに変更した

--- Python/upload.py
+++ Python/upload.py
@@ -34,9 +34,6 @@ for f in files2:
     f = f.replace('\\', '/')
     f = f.replace("D:/htdocs/", "../")
     cur.execute("INSERT yolo_video_table VALUES (%(id)s, %(frame1)s, %(frame2)s, %(ball_id)s, %(player_id)s, %(ans_id)s, %(x_coordinate)s, %(y_coordinate)s, %(video_path)s, %(yolo_flag)s)", {'id':None, 'ans_id':-1, 'ball_id':1, 'frame1':-1, 'frame2':-1, 'player_id':1, 'video_path':f, 'x_coordinate':-1, 'y_coordinate':-1, 'yolo_flag':0})
-# for file in files:
-#    cur.execute("INSERT yolo_video_table VALUES (%(id)s, %(ans_id)s, %(ball_id)s, %(frame1)s, %(frame2)s, %(player_id)s, %(x_coordinate)s, %(y_coordinate)s, %(video_path)s, %(yolo_flag)s)", {'id':None, 'ans_id':-1, 'ball_id':1, 'frame1':-1, 'frame2':-1, 'player_id':1, 'video_path':file, 'x_coordinate':-1, 'y_coordinate':-1, 'yolo_flag':0})
-
 print(str(files2) + ' was uploaded!')
 
 cur.close()
