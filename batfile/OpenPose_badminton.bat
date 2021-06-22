1294fc0e3c57fe94d4eabe3f1263447ed7a83f28
yamakit <yamakitta@gmail.com>
2021-06-22 09:30:25 +0900

    trackingをONにした

--- batfile/OpenPose_badminton.bat
+++ batfile/OpenPose_badminton.bat
@@ -4,7 +4,7 @@ rem ********今%1になっている所を動画と同じ名前に置換する***
 cd C:\openpose
 
 rem OpenPose実行
-bin\OpenPoseDemo.exe --video "D:\htdocs\2021SEN_KEN\badminton\1\IMG_%1.MOV" --number_people_max 1 --tracking 0 --write_video D:\htdocs\2021SEN_KEN\badminton\1\IMG_%1.avi --write_json "D:\htdocs\2021SEN_KEN\badminton\1\openpose_%1"
+bin\OpenPoseDemo.exe --video "D:\htdocs\2021SEN_KEN\badminton\1\IMG_%1.MOV" --number_people_max 1 --tracking 1 --write_video D:\htdocs\2021SEN_KEN\badminton\1\IMG_%1.avi --write_json "D:\htdocs\2021SEN_KEN\badminton\1\openpose_%1"
 
 rem フォルダを作成
 mkdir D:\htdocs\2021SEN_KEN\badminton\1\%1
