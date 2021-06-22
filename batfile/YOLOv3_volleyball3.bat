4bb641b72d1c2b1adc4ecd4ebebb1089ff4047be
yamakit <yamakitta@gmail.com>
2021-06-22 13:38:17 +0900

    不要な部分を削除した

--- batfile/YOLOv3_volleyball3.bat
+++ batfile/YOLOv3_volleyball3.bat
@@ -1,7 +1,4 @@
 @echo off 
-rem ******今%1になっているところを動画と同じ名前にリネームしておくこと。******
-rem ******今0604になっているところは今日の日付にしておくこと。******************
-
 rem Dドライブへ移動
 cd /d D:
 
@@ -24,10 +21,4 @@ rem darknetを動かすフォルダへの移動
 cd C:\darknet\build\darknet\x64
 
 rem darknetの実行
-darknet detector test "C:\darknet\build\darknet\x64\cfg\volleyballdetect.data" "C:\darknet\build\darknet\x64\cfg\yolov3-spp_volleyballdetect.cfg" "C:\darknet\build\darknet\x64\yolov3-spp_volleyballdetect_final.weights" < "D:\htdocs\2021SEN_KEN\volleyball\1\ffmpeg_%1.txt" -dont_show -thresh 0.9 -ext_output -out "D:\htdocs\2021SEN_KEN\volleyball\1\json\ffmpeg_%1.json
-
-rem cd C:\openpose
-rem mkdir D:\htdocs\2021SEN_KEN\volleyball\1\openpose_%1
-rem bin\OpenPoseDemo.exe --video "D:\htdocs\2021SEN_KEN\volleyball\1\IMG_%1.MOV" --number_people_max 1 --tracking 0 --write_video D:\htdocs\2021SEN_KEN\volleyball\1\IMG_%1.avi --write_json "D:\htdocs\2021SEN_KEN\volleyball\1\openpose_%1"
-rem mkdir openpose_%1
-rem ffmpeg -i "D:\htdocs\2021SEN_KEN\volleyball\1\IMG_%1.avi" -r 30 D:\htdocs\2021SEN_KEN\volleyball\1\openpose_%1\%%06d.jpg 
+darknet detector test "C:\darknet\build\darknet\x64\cfg\volleyballdetect.data" "C:\darknet\build\darknet\x64\cfg\yolov3-spp_volleyballdetect.cfg" "C:\darknet\build\darknet\x64\data\volleyballdetect\yolov3-spp_volleyballdetect_best.weights" < "D:\htdocs\2021SEN_KEN\volleyball\1\ffmpeg_%1.txt" -dont_show -thresh 0.8 -ext_output -out "D:\htdocs\2021SEN_KEN\volleyball\1\ffmpeg_%1.json
