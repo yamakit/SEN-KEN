940dc65463f5eed53925314d7873e3f1353ba4b3
yamakit <yamakitta@gmail.com>
2021-06-22 09:10:12 +0900

    不要な箇所を削除した

--- batfile/OpenPose_volleyball.bat
+++ batfile/OpenPose_volleyball.bat
@@ -1,24 +1,14 @@
 @echo off
 
-rem ********今0203になっている所を動画と同じ名前に置換する********
-
-rem ダウンロードのフォルダに移動
-rem cd "C:\Users\procon\D
-rem ダウンロードのフォルダにファイルを移動
-rem move IMG_0203.MOV D:\2021SEN_KEN\volleyball\0514 
-
-
-rem Openposeのフォルダに移動
+rem ********今%1になっている所を動画と同じ名前に置換する********
 cd C:\openpose
 
-rem 出力したjsonが入るフォルダを作る
-rem mkdir D:\htdocs\2021SEN_KEN\volleyball\1\openpose_0203
-
 rem OpenPose実行
-bin\OpenPoseDemo.exe --video "D:\htdocs\2021SEN_KEN\volleyball\1\IMG_0203.MOV" --number_people_max 1 --tracking 0 --write_video D:\htdocs\2021SEN_KEN\volleyball\1\IMG_0203.avi --write_json "D:\htdocs\2021SEN_KEN\volleyball\1\openpose_0203"
+bin\OpenPoseDemo.exe --video "D:\htdocs\2021SEN_KEN\volleyball\1\IMG_%1.MOV" --number_people_max 1 --tracking 0 --write_video D:\htdocs\2021SEN_KEN\volleyball\1\IMG_%1.avi --write_json "D:\htdocs\2021SEN_KEN\volleyball\1\openpose_%1"
 
 rem フォルダを作成
-mkdir openpose_0203
+mkdir D:\htdocs\2021SEN_KEN\volleyball\1\%1
 
 rem 動画を分割
-ffmpeg -i "D:\htdocs\2021SEN_KEN\volleyball\1\IMG_0203.avi" -r 30 D:\htdocs\2021SEN_KEN\volleyball\1\openpose_0203\%%06d.jpg
+ffmpeg -i "D:\htdocs\2021SEN_KEN\volleyball\1\IMG_%1.avi" -r 30 D:\htdocs\2021SEN_KEN\volleyball\1\%1\%%06d.jpg
+
