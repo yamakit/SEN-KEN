a6dddf123447679824eadf9a29fc388c72ba9a03
yamakit <yamakitta@gmail.com>
2021-07-07 09:25:47 +0900

    DBをtestに変更

--- JavaScript/view.js
+++ JavaScript/view.js
@@ -1,10 +1,3 @@
-
-// function drawVideo() {
-//     var video = document.getElementById("mv");
-//     // var canvas = document.getElementById("c");
-//     // canvas.getContext("2d").drawImage(video, 0, 0, 480, 270);
-// }
-
 window.onload = function () {
     var data = location.href.split("?")[1];
     var text = data.split("=")[1];
