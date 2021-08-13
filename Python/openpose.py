1ffe395fb606076fc4a1ca9833c5496d952aa47e
yamakit <yamakitta@gmail.com>
2021-08-13 14:03:48 +0900

    yolo_flag=2のものにOpenposeをかける

--- Python/openpose.py
+++ Python/openpose.py
@@ -26,18 +26,21 @@ rep_chk = 0
 print(folder)
 
 # folder = ['D:\\htdocs\\SEN-KEN\\2021SEN_KEN\\volleyball\\02\\2106041922040.MOV']
+# folder = gb.glob("D:\\htdocs\\SEN-KEN\\2021SEN_KEN\\volleyball\\*\\*.MOV") #ローカル環境での実行用、運用時は消去
+
 
 for fl in folder:
-    print(fl + 'を処理中...')
+    print(fl + 'を処理中...')   
 
     file_name = fl.replace(".MOV","")
     file_names = file_name.replace("../", "D:\\htdocs\\")
-    file_names = file_names.replace("D:\\htdocs\\SEN-KEN\\2021SEN_KEN\\volleyball\\","")
+    file_names = file_names.replace("D:\\htdocs\\SEN-KEN/2021SEN_KEN/volleyball/","")
+    print(file_names)
     person_num = file_names[:2]
     print(person_num)
     video_num = file_names[3:]
     print(video_num)
-    resutl = subprocess.run(['C:/Users/procon/Desktop/2021SEN-KEN/OpenPose_volleyball.bat', video_num , person_num], shell=True)
+    resutl = subprocess.run(['OpenPose_volleyball.bat', person_num , video_num], shell=True)
 
     conn = mydb.connect(host='localhost',port='3306',user='root',password='',database='SEN-KEN')
     cur = conn.cursor(buffered=True)
