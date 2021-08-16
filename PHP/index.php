bfdefc82f6d251e1474ce2b412d2ad596e96b1d8
yamakit <yamakitta@gmail.com>
2021-08-16 10:13:34 +0900

    テスト用のSQLを追加した

--- PHP/index.php
+++ PHP/index.php
@@ -12,8 +12,12 @@ try {
 
 // echo "<p>DB接続に成功しました。</p>";
 
-$sql = "SELECT * FROM `yolo_video_table`";
-// $sql = "SELECT * FROM `yolo_video_table` ORDER BY RAND()";
+//$sql = "SELECT * FROM `yolo_video_table`";
+//テスト用
+//$sql = "SELECT * FROM `yolo_video_table` WHERE video_id BETWEEN 378 AND 417 ORDER BY RAND()";
+//$sql = "SELECT * FROM `yolo_video_table` WHERE video_id = 105";
+$sql = "SELECT * FROM `yolo_video_table` where ball_id = 1 ORDER BY RAND()";
+
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 $result = $stmt->fetchAll();
