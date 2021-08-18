71fb9f39156cbf1addb5d13466bdeea59216e42f
yamakit <yamakitta@gmail.com>
2021-08-18 17:51:51 +0900

    trackingのオプションを追加する

--- Python/OpenPose_volleyball.bat
+++ Python/OpenPose_volleyball.bat
@@ -5,7 +5,7 @@ rem ********今%1になっている所を動画と同じ名前に置換する***
 cd C:\openpose
 
 rem OpenPose実行
-bin\OpenPoseDemo.exe --video "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\%2.MOV" --write_video "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\%2.avi" --write_json "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\openpose_%2"
+bin\OpenPoseDemo.exe --video "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\%2.MOV" --tracking 0 --write_video "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\%2.avi" --write_json "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\openpose_%2" --number_people_max 1
 echo bin\OpenPoseDemo.exe --video "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\%2.MOV" --write_video "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\%2.avi" --write_json "D:\htdocs\SEN-KEN\2021SEN_KEN\volleyball\%1\openpose_%2"
 
 rem フォルダを作成
