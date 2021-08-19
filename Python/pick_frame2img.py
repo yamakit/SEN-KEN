e6e6d58d1d0c0b611bf4ec08cbc7df17f83ac8cf
Satonaka21 <Jagakiko0411@icloud.com>
2021-08-19 12:08:43 +0900

    pick_frame2img.pyで、DBにframe1のパスなどを送る処理を実装

--- Python/pick_frame2img.py
+++ Python/pick_frame2img.py
@@ -3,7 +3,7 @@ import os
 
 conn = mydb.connect(host='localhost',port='3306',user='root',password='',database='test')
 cur = conn.cursor(buffered=True)
-cur.execute("SELECT video_path,frame2 FROM yolo_video_tables WHERE yolo_flag = 2")
+cur.execute("SELECT video_path,frame1 FROM yolo_video_tables WHERE yolo_flag = 2")
 rows = cur.fetchall()
 path_lists = [] #目的の画像の連番とそれが入っているフォルダーのパスのリスト群
 
@@ -15,21 +15,21 @@ conn.close()
 
 for path_list in path_lists:
     folder = path_list[0]
-    frame2 = path_list[1]
+    frame1 = path_list[1]
 
     folder_path = folder.replace('.MOV', '/')                       #フォルダ化
     folder_path = folder_path.replace('../', 'D:/htdocs/SEN-KEN/')  #フルパス化
 
-    image_num = str(frame2).zfill(5)    #目的の画像に振られる連番に合わせて5桁で0埋め
+    image_num = str(frame1).zfill(6)    #目的の画像に振られる連番に合わせて5桁で0埋め
 
     image_path = folder_path + image_num + '.jpg' #目的の画像のパスを生成
     print(image_path, end=' ')
 
     if(os.path.exists(image_path)):
         #---生成したパスをDBに送る---
-        conn = mydb.connect(host='localhost',port='3306',user='root',password='',database='test')
+        conn = mydb.connect(host='localhost',port='3306',user='root',password='',database='sen-ken')
         cur = conn.cursor(buffered=True)
-        cur.execute(f"UPDATE `yolo_video_tables` SET `frame2の画像のパスを入れるカラム` = '{image_path}' WHERE `video_path` = '{folder}'")
+        cur.execute(f"INSERT INTO `picture_table`(`picture_path`,`frame1`) VALUES ('{image_path}',{frame1})")
         cur.close
         conn.commit()
         conn.close()
