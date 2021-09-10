30f0672716afd3783eb1633a86d7a0e8328f5f44
yamakit <yamakitta@gmail.com>
2021-09-10 15:32:51 +0900

    insertするカラムにvideo_idを追加した

--- Python/pick_frame1img.py
+++ Python/pick_frame1img.py
@@ -1,14 +1,14 @@
 import mysql.connector as mydb
 import os
 
-conn = mydb.connect(host='localhost',port='3306',user='root',password='',database='test')
+conn = mydb.connect(host='localhost',port='3306',user='root',password='',database='sen-ken')
 cur = conn.cursor(buffered=True)
-cur.execute("SELECT video_path,frame1 FROM yolo_video_tables WHERE yolo_flag = 2")
+cur.execute("SELECT video_path,frame1,video_id FROM yolo_video_table WHERE 1")
 rows = cur.fetchall()
 path_lists = [] #目的の画像の連番とそれが入っているフォルダーのパスのリスト群
 
 for row in rows:
-    path_lists.append([row[0], row[1]])
+    path_lists.append([row[0], row[1], row[2]])
 cur.close
 conn.commit()
 conn.close()
@@ -16,9 +16,10 @@ conn.close()
 for path_list in path_lists:
     folder = path_list[0]
     frame1 = path_list[1]
+    video_id = path_list[2]
 
     folder_path = folder.replace('.MOV', '/')                       #フォルダ化
-    folder_path = folder_path.replace('../', 'D:/htdocs/SEN-KEN/')  #フルパス化
+    folder_path = folder_path.replace('../', '../')  #フルパス化
 
     image_num = str(frame1).zfill(6)    #目的の画像に振られる連番に合わせて5桁で0埋め
 
@@ -29,7 +30,7 @@ for path_list in path_lists:
         #---生成したパスをDBに送る---
         conn = mydb.connect(host='localhost',port='3306',user='root',password='',database='sen-ken')
         cur = conn.cursor(buffered=True)
-        cur.execute(f"INSERT INTO `picture_table`(`picture_path`,`frame1`) VALUES ('{image_path}',{frame1})")
+        cur.execute(f"INSERT INTO `picture`(`video_id`,`frame1`, `picture_path`) VALUES ({video_id}, {frame1},'{image_path}')")
         cur.close
         conn.commit()
         conn.close()
